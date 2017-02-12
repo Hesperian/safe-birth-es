e63ae9e05fea94002f06cb80726c7c070d177fe6
Matthew Litwin <mlitwin@kcptech.com>
2017-02-12 13:42:09 -0800

    updated build

--- hesperian_mobile.js
+++ hesperian_mobile.js
@@ -134,11 +134,12 @@ $("div:jqmData(role='page')").live("pagebeforeshow",function(event, ui) {
 
 $("div:jqmData(role='page')").live("pageshow",function(event) {
   var thisPage = $(this).attr("id");
-/*
-  if(HM.gaAvailable()) {
-    window.ga.trackView(thisPage);
-  }
-  */
+
+  try {
+    if(HM.gaAvailable()) {
+      window.ga.trackView(thisPage);
+    }
+  } catch(err) {}
 
 	if ($(this).attr("swipe") == "true") {
 		var el = $("div.sequence-bar-bottom",this);
